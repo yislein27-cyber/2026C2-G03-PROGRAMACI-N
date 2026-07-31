44efde90038b25cc7e54e89f4c2a3f4e1265892a
yislein27-cyber <yislein27@gmail.com>
2026-07-30 18:58:48 -0600

    s112

--- Clase 11/app.py
+++ Clase 11/app.py
@@ -1,4 +1,14 @@
 """Programa principal del proyecto modular BCCR."""
 
-import matplotlib.pyplot as plt
+from lectura_datos import cargar_tabla_bccr
+from limpieza_datos import limpiar_datos
+
+def ejecutar():
+    """"Cargar los datos y presentar el menú del sistema"""
+    datos_crudos = cargar_tabla_bccr()
+    datos = limpiar_datos(datos_crudos)
+    print(datos.head())
+    
+    if __name__ == "__main__":
+        ejecutar()
 
